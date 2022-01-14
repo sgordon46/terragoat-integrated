085f73f621048f589482f669cd185475cfb97ed1
sgordon46 <sgordon@paloaltonetworks.com>
2022-01-14 11:16:07 -0600

    Update s3.tf
    Co-authored-by: prisma-cloud-devsecops[bot] <89982750+prisma-cloud-devsecops[bot]@users.noreply.github.com>

--- s3.tf
+++ s3.tf
@@ -26,6 +26,13 @@ resource "aws_s3_bucket" "data" {
       }
     }
   }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
 resource "aws_s3_bucket_object" "data_object" {
